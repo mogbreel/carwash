50188a6c195f2630792c5d1ed7b996576977d969
unknown <mohammedxp2020@gmail.com>
2025-03-15 15:37:22 +0200

    Some changes on db file..

--- client/models.py
+++ client/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # CLIENTS TABLE IN DATABASE
 class Client(models.Model):
     name = models.CharField(max_length=100, verbose_name='إسم العميل')
-    vehicle_plate = models.IntegerField(verbose_name='رقم لوحة السيارة')
+    vehicle_plate = models.CharField(max_length=10, verbose_name='رقم لوحة السيارة')
     phone = models.CharField(max_length=15, verbose_name='رقم الجوال')
     washcount = models.IntegerField(default=0, verbose_name='عدد الغسلات')
 
